47a6a713fbb83e5a0dc7851ba6319fad092f6c03
Ubuntu <ubuntu@ip-172-31-20-167.us-west-2.compute.internal>
2013-07-10 12:00:59 +0000

    remodification web.js

--- web.js
+++ web.js
@@ -2,12 +2,17 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
- fs.readFile('/index', function (err, data) {
- if (err) throw err;
- console.log(data);
- buffer.toString();
-}});
+app.get(
+fs.readFile(__dirname + '/bitstarter/index.html', 'utf8', function(err, html){
+ if(err){
+   console.log(err);
+  }else{
+     connection.sentUTF( JSON.stringify({
+            content: html, 
+            data: {}
+        }));
+      }
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
