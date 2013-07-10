03ab95493749668df9970fab2007e97dc45756f7
Ubuntu <ubuntu@ip-172-31-20-167.us-west-2.compute.internal>
2013-07-10 12:56:57 +0000

    web.js2

--- web.js
+++ web.js
@@ -6,9 +6,9 @@ server = http.createServer(function(req, res{
 
 fs.readFile(__dirname + '/index.html', 'utf8', function(err, data){
  if(err) throw err;
- res.write(data, 'utf8');
- res.end();
-});
+ len = res.write(data, 'utf8');
+ console.log(len + "bytes: " + buf.toString('utf8', 0, len));
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
